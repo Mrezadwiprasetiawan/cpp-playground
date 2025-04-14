375d190631dad99eeec51caaa63cd5bac35ede66
Mrezadwiprasetiawan <mrezadwiprasetiawan@gmail.com>
2025-04-14 16:08:07 +0700

    optimizing prime class

--- number_system/src/prime.cxx
+++ number_system/src/prime.cxx
@@ -1,55 +1,66 @@
-#include "numbers.hxx"
 #include <cctype>
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 
-#define USING_OSTREAM                                                          \
-  using std::cout;                                                             \
+#include "numbers.hxx"
+
+#define USING_OSTREAM \
+  using std::cout;    \
   using std::endl;
 
 std::string argname[] = {"-h", "-help", "-l", "-limit", "-s", "-size"};
 
-uint64_t to_number(char *number) {
-  using std::runtime_error;
-  uint64_t result = 0;
-  size_t len = strlen(number);
-  for (size_t i = 0; i < len; ++i) {
-    char c = number[i];
-    if (!std::isdigit(c)) {
-      throw runtime_error("Invalid character '" + std::string(1, c) +
-                          "' in number.");
-    }
-    result = result * 10 + (c - '0');
+uint64_t to_number_with_suffix(const char *str) {
+  size_t len = strlen(str);
+  uint64_t num = 0;
+  size_t i = 0;
+
+  while (i < len && std::isdigit(str[i])) {
+    num = num * 10 + (str[i++] - '0');
   }
-  return result;
+
+  if (i == len) return num;
+
+  char suffix = std::tolower(str[i]);
+  switch (suffix) {
+    case 'k':
+      return num << 10;
+    case 'm':
+      return num << 20;
+    case 'g':
+      return num << 30;
+    case 't':
+      return num << 40;
+    case 'p':
+      return num << 50;
+    case 'e':
+      return num << 60;
+    default:
+      std::cerr << "Invalid suffix: " << str[i] << std::endl;
+      std::exit(1);
+  }
 }
 
 void printHelp() {
   USING_OSTREAM;
   cout << "Print prime number sequences" << endl;
   cout << "\t-h -help\tprint this help" << endl;
-  cout << "\t-l -limit <number>\tprint prime numbers up to limit using sieve "
-          "of eratosthenes"
+  cout << "\t-l -limit <number>\tprint primes up to limit (supports K/M/G)"
        << endl;
-  cout << "\t-s -size <number>\tprint first n prime numbers using optimized "
-          "trial division"
-       << endl;
+  cout << "\t-s -size <number>\tprint first N primes (supports K/M/G)" << endl;
 }
 
 void do_l(uint64_t limit) {
   USING_OSTREAM;
   Prime<uint64_t> prime;
-  for (uint64_t prime : prime.from_range_limit(limit))
-    cout << prime << endl;
+  for (uint64_t p : prime.from_range_limit(limit)) cout << p << endl;
 }
 
 void do_s(size_t size) {
   USING_OSTREAM;
   Prime<uint64_t> prime;
-  for (uint64_t prime : prime.from_size(size))
-    cout << prime << endl;
+  for (uint64_t p : prime.from_size(size)) cout << p << endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -82,29 +93,23 @@ int main(int argc, char *argv[]) {
 
   if (arg_l != -1 && arg_s != -1) {
     std::cerr << "Error: Cannot specify both -l and -s options" << std::endl;
-    printHelp();
     return 1;
   }
 
-  try {
-    if (arg_l != -1) {
-      if (arg_l + 1 >= argc) {
-        std::cerr << "Error: Missing argument for -l option" << std::endl;
-        return 1;
-      }
-      do_l(to_number(argv[arg_l + 1]));
-    } else if (arg_s != -1) {
-      if (arg_s + 1 >= argc) {
-        std::cerr << "Error: Missing argument for -s option" << std::endl;
-        return 1;
-      }
-      do_s(to_number(argv[arg_s + 1]));
-    } else {
-      printHelp();
+  if (arg_l != -1) {
+    if (arg_l + 1 >= argc) {
+      std::cerr << "Error: Missing argument for -l option" << std::endl;
       return 1;
     }
-  } catch (const std::exception &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
+    do_l(to_number_with_suffix(argv[arg_l + 1]));
+  } else if (arg_s != -1) {
+    if (arg_s + 1 >= argc) {
+      std::cerr << "Error: Missing argument for -s option" << std::endl;
+      return 1;
+    }
+    do_s(to_number_with_suffix(argv[arg_s + 1]));
+  } else {
+    printHelp();
     return 1;
   }
 
