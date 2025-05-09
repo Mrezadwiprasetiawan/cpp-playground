8379c5fb7406a24e9952b44fe74679a0141b5b6b
Mrezadwiprasetiawan <mrezadwiprasetiawan@gmail.com>
2025-05-09 12:02:21 +0700

    update lupa diselesain fungsi kombinasinya

---
+++ discrete/include/combination.hxx
@@ -0,0 +1,8 @@
+#pragma once
+#include <type_traits>
+
+template <typename T>
+std::enable_if<std::is_arithmetic<T>::value,T>
+combination( T n, T k){
+  return factorial(n)/(factorial(k) * (factorial(n-k));
+}
