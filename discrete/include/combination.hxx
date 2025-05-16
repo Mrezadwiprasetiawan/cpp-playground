8804cb9ba1647f7d41867570cbff3a19c3473c2f
M Reza Dwi Prasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2025-05-16 14:23:37 +0700

    Temporary optimize combination.hxx
    just a template to remember that when i want to include big_int header, the logic was look like this

--- discrete/include/combination.hxx
+++ discrete/include/combination.hxx
@@ -3,7 +3,10 @@
 #include <factorial.hxx>
 
 template <typename T>
-std::enable_if<std::is_arithmetic<T>::value,T>
+std::enable_if_t<std::is_arithmetic<T>::value,T>
 combination( T n, T k){
-  return factorial(n)/(factorial(k) * (factorial(n-k));
+  T res = 1;
+  T maxK = k > n/2 ? k : n - k;
+  for(T i = maxK + 1; i <= n; ++i) res *= i;
+  return res/factorial(n - maxK);
 }
