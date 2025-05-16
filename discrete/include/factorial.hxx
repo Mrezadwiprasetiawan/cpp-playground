f43009c4b7232f550bd35348913fe8ec88715376
M Reza Dwi Prasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2025-05-16 22:48:19 +0700

    tiny fix factorial.hxx

--- discrete/include/factorial.hxx
+++ discrete/include/factorial.hxx
@@ -2,7 +2,7 @@
 #include <type_traits>
 
 template <typename T>
-std::enable_if<std::is_arithmetic_v<T>> factorial(T n) {
+std::enable_if_t<std::is_arithmetic_v<T>> factorial(T n) {
   if (n == 1) return 1;
   return n * factorial(n - 1);
 }
