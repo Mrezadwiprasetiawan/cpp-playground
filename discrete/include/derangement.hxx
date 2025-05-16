1442ad130390f6fc2ac57d3eb142b48cae34aede
M Reza Dwi Prasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2025-05-16 22:47:33 +0700

    tiny fix  derangement.hxx

--- discrete/include/derangement.hxx
+++ discrete/include/derangement.hxx
@@ -2,7 +2,7 @@
 #include <type_traits>
 
 template <typename T, typename Ret = uint64_t>
-std::enable_if<std::is_arithmetic_v<T>, Ret>
+std::enable_if_t<std::is_arithmetic_v<T>, Ret>
 derangement(T n){
   if (!n) return 1;
   if (n==1) return 0;
