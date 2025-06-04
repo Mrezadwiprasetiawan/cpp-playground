79a82e5938565d287fce0001590ca50a41d45a34
Mrezadwiprasetiawan <mrezadwiprasetiawan@gmail.com>
2025-06-05 04:03:43 +0700

    fix and add cmake preset for windows
    iam suck with the cmake on windows

--- number_system/src/big_int.cxx
+++ number_system/src/big_int.cxx
@@ -17,22 +17,21 @@
   along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-
 #include <big_int.hxx>
 #include <iostream>
 
-void test_add(const big_int& a, const big_int& b) {
+void test_add(const Big_int& a, const Big_int& b) {
   using namespace std;
   cout << "a + b :" << endl;
   cout << "a = " << a << endl;
   cout << "b = " << b << endl;
-  big_int c = a + b;
+  Big_int c = a + b;
   cout << "c = " << c << endl;
 }
 
 int main() {
-  big_int a(100);
-  big_int b(-101);
+  Big_int a(100);
+  Big_int b(-101);
   test_add(a, b);
 
   a = (1ULL << 63);
