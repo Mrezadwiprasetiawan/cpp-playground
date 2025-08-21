cc86baab2b103836970c2e720a07589207be7ae4
Mrezadwiprasetiawan <mrezadwiprasetiawan@gmail.com>
2025-08-22 06:35:23 +0700

    add debug realpath on the original collatz function

--- collatz/src/collatz.cpp
+++ collatz/src/collatz.cpp
@@ -10,18 +10,18 @@
 int main(int argc, const char **argv) {
   using namespace std;
   unordered_map<string, string> args;
-  args = get_args(argc, argv);
+  args                       = get_args(argc, argv);
   uint64_t highestOddStarter = 0, longestPath = 0, seed = 0;
   if (args.empty() || args.find("help") != args.end() || args.find("h") != args.end()) return 0;
 
   try {
     highestOddStarter = stoull(args.at("highestOddStarter"));
-    seed = stoull(args.at("seed"));
-    longestPath = stoull(args.at("longestPath"));
+    seed              = stoull(args.at("seed"));
+    longestPath       = stoull(args.at("longestPath"));
   } catch (const out_of_range &ofer) { cout << ofer.what() << endl; }
 
-  try { 
-    CollatzMatrix colMat(highestOddStarter, longestPath);
+  try {
+    CollatzMatrix          colMat(highestOddStarter, longestPath);
     vector<Vec4<uint64_t>> path = std::move(colMat.find_path(seed));
 
     if (args.find("debug") != args.end() && args.at("debug") == "true") colMat.printMatrices();
@@ -29,6 +29,9 @@ int main(int argc, const char **argv) {
     for (auto val : path)
       cout << setw(10) << "Matrix index = " << val.w << setw(10) << "row = " << val.x << setw(10) << "col = " << val.y << setw(10) << "value = " << val.z
            << endl;
+    cout << endl << "Real Path on the original collatz function =";
+    for (auto val : path) cout << 2 * val.z - 1 << " ";
+    cout << endl;
   } catch (const exception &e) { cout << e.what() << endl; }
 
   return 0;
