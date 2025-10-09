43cc2a4383eed0be351f2c204256beee617d2da2
Mrezadwiprasetiawan <mrezadwiprasetiawan@gmail.com>
2025-10-09 18:53:33 +0700

    update

--- microcontroller-sim/src/test_sim.cxx
+++ microcontroller-sim/src/test_sim.cxx
@@ -10,7 +10,7 @@ void simulate(Board& board) {
   using namespace std::chrono_literals;
 
   auto now = steady_clock::now();
-  while (steady_clock::now() - now < 10s) {
+  while (steady_clock::now() - now < 2s) {
     board.digitalWrite(0, HIGH);
     board.analogWrite(1, 128);
   }
@@ -21,10 +21,10 @@ void debugger(std::vector<Pin>& pins) {
   using namespace std::chrono_literals;
 
   auto now = steady_clock::now();
-  while (steady_clock::now() - now < 10s) {
+  while (steady_clock::now() - now < 2s) {
     for (int i = 0; i < PINSIZE; ++i) std::cout << "Pin " << i << ": " << (int)pins[i].get_value() << '\n';
     std::cout << "-------------------\n";
-    std::this_thread::sleep_for(100ms);
+    std::this_thread::sleep_for(10ms);
   }
 }
 
